d992327660bbe013460a677c3d60a0c3e1129f71
jchinedu <jchinedu080@gmail.com>
2025-06-06 22:27:26 +0100

    'process'

--- python/menstrual_app.py
+++ python/menstrual_app.py
@@ -16,3 +16,7 @@ def print_period_flow_dates(start_day, start_month, start_year):
     for i in range(5):
         day, month, year = add_days(start_day, start_month, start_year, i)
         print(f"Day {i+1}: {day:02d}-{month:02d}-{year}")
+def print_ovulation_period(start_day, start_month, start_year):
+    start = add_days(start_day, start_month, start_year, 12)
+    end = add_days(start_day, start_month, start_year, 16)
+    print(f"Ovulation Period: Start: {start[0]:02d}-{start[1]:02d}-{start[2]} End: {end[0]:02d}-{end[1]:02d}-{end[2]}")

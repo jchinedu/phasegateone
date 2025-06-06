3e28877fa387719162115f94d8bdbd81e7525e9b
jchinedu <jchinedu080@gmail.com>
2025-06-06 22:24:41 +0100

    'test2'

--- python/test_menstrual_app.py
+++ python/test_menstrual_app.py
@@ -9,4 +9,6 @@ class TestMenstrualCycle(unittest.TestCase):
         self.assertTrue(is_valid_date(15, 6, 2025))
         self.assertFalse(is_valid_date(31, 2, 2025))
 
-    
+    def test_leap_year(self):
+        self.assertTrue(is_leap_year(2024))
+        self.assertFalse(is_leap_year(2023))

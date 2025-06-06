59962dd094827a0bce91de8bd062619108cd1104
jchinedu <jchinedu080@gmail.com>
2025-06-06 22:06:18 +0100

    'process'

--- python/menstrual_app.py
+++ python/menstrual_app.py
@@ -9,3 +9,5 @@ def is_valid_date(day, month, year):
 def add_days(day, month, year, days_to_add):
     date = datetime(year, month, day) + timedelta(days=days_to_add)
     return date.day, date.month, date.year
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)

b350a4b3dad70e41295bf1dc768370a80f06f6ee
jchinedu <jchinedu080@gmail.com>
2025-06-08 14:17:02 +0100

    'today'

--- python/semi_colon_store_check_out.py
+++ python/semi_colon_store_check_out.py
@@ -12,3 +12,16 @@ def get_valid_float(prompt):
             return float(input(prompt))
         except ValueError:
             print("Invalid input. Enter a valid number: ")
+def print_header():
+    print("====================================================")
+    print("SEMICOLON STORES")
+    print("MAIN BRANCH")
+    print("LOCATION: 312, HERBERT MACAULY WAY, SABO YABA, LAGOS.")
+    print("Tel: +123 456 7890")
+    print("Date:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+def handle_payment(total_due):
+    print("Select payment method:")
+    print("1. Cash")
+    print("2. Card")
+    print("3. Mobile Money")
+    payment_choice = get_valid_int("Enter your choice: ")

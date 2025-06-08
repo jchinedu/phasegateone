2c7178c3ecafcc84f3451b92301f0498917cc5b6
jchinedu <jchinedu080@gmail.com>
2025-06-08 04:49:15 +0100

    'progress'

--- Java/SemiColonStoreCheckout.java
+++ Java/SemiColonStoreCheckout.java
@@ -2,4 +2,61 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-public class SemiColonStoreCheckout {}
+public class SemiColonStoreCheckout {
+public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}

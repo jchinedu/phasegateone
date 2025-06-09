6a428a2d9f075e083b3a6ad2d12e5befad0194da
jchinedu <jchinedu080@gmail.com>
2025-06-09 12:24:41 +0100

    'progress'

--- Java/CreditCardValidatorTest.java
+++ Java/CreditCardValidatorTest.java
@@ -7,6 +7,11 @@ public class CreditCardValidatorTest {
 @Test
     public void testIsValidLengthAndDigits() {
         assertTrue(CreditCardValidator.validLength("4539876543210124"));
-        //assertFalse(CreditCardValidator.isValidLengthAndDigits("4539a76543210124")); 
+        assertFalse(CreditCardValidator.validLength("12345")); 
+}
+
+@Test
+    public void cardType() {
+        assertEquals("VISA", CreditCardValidator.cardType("4539876543210124"));
 }
 }

ab28868c2d3b1909a99b8a37f92d0d467870f1d9
jchinedu <jchinedu080@gmail.com>
2025-06-10 06:16:45 +0100

    'today'

--- python/test_credit_card_validator.py
+++ python/test_credit_card_validator.py
@@ -4,4 +4,4 @@ from credit_card_validator import valid_length, card_type, check
 class TestCreditCardValidator(unittest.TestCase):
 	def test_valid_length(self):
         	self.assertTrue(valid_length("1234567890123456"))
-
+	        self.assertFalse(valid_length("123456789012345"))

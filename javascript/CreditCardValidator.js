1e11c9c7598207365d2ce0d55253eed78da30082
jchinedu <jchinedu080@gmail.com>
2025-06-10 05:05:07 +0100

    'progress'

--- javascript/CreditCardValidator.js
+++ javascript/CreditCardValidator.js
@@ -4,3 +4,11 @@ function ValidLength(cardNumber) {
   if (cardNumber.length !== 16) {
     return false;
   }
+for (let i = 0; i < cardNumber.length; i++) {
+    let ch = cardNumber[i];
+    if (ch < '0' || ch > '9') {
+      return false;
+    }
+  }
+  return true;
+}

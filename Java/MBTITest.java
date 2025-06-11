ce730e5e260fe498bc85c131bd1e405d930cd739
jchinedu <jchinedu080@gmail.com>
2025-06-11 19:18:06 +0100

    'progress'

--- Java/MBTITest.java
+++ Java/MBTITest.java
@@ -3,5 +3,6 @@ public class MBTITest{
 	static String[][] questions = {
 		{"Expend energy, enjoy groups", "Conserve energy, enjoy one-on-one"},
 		{"Interpret literally", "Look for meaning and possibilities"},
-        {"Logical, thinking, questioning", "Empathetic, feeling, accommodating"},
-	 {"Organized, orderly", "Flexible, adaptable"},
+                {"Logical, thinking, questioning", "Empathetic, feeling, accommodating"},
+	        {"Organized, orderly", "Flexible, adaptable"},
+		{"More outgoing, think out loud", "More reserved, think to yourself"},

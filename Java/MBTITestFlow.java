f2d39931fca3510ebe2ddd8c1e2097e4f54adc47
jchinedu <jchinedu080@gmail.com>
2025-06-11 23:31:46 +0100

    'progress'

---
+++ Java/MBTITestFlow.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MBTITestFlow {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        showWelcomeMessage();

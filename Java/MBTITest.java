1df5a3a2735fd9380d73bcb0fcd1d7f038998406
jchinedu <jchinedu080@gmail.com>
2025-06-11 19:17:20 +0100

    'progress'

--- Java/MBTITest.java
+++ Java/MBTITest.java
@@ -4,3 +4,4 @@ public class MBTITest{
 		{"Expend energy, enjoy groups", "Conserve energy, enjoy one-on-one"},
 		{"Interpret literally", "Look for meaning and possibilities"},
         {"Logical, thinking, questioning", "Empathetic, feeling, accommodating"},
+	 {"Organized, orderly", "Flexible, adaptable"},

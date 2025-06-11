1cbafaea0a1c8ea32c1d005a77fcb7f70b0f161e
jchinedu <jchinedu080@gmail.com>
2025-06-12 00:46:41 +0100

    'progress

--- python/mbtitest.py
+++ python/mbtitest.py
@@ -2,3 +2,4 @@ QUESTIONS = [
 	("Expend energy, enjoy groups", "Conserve energy, enjoy one-on-one"),
 	("Interpret literally", "Look for meaning and possibilities"),
 	("Logical, thinking, questioning", "Empathetic, feeling, accommodating"),
+	("Organized, orderly", "Flexible, adaptable"),

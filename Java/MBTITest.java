d5019af107e820e18d0cbf7bdd5e1e529f89f82c
jchinedu <jchinedu080@gmail.com>
2025-06-11 19:18:35 +0100

    'progress'

--- Java/MBTITest.java
+++ Java/MBTITest.java
@@ -6,3 +6,5 @@ public class MBTITest{
                 {"Logical, thinking, questioning", "Empathetic, feeling, accommodating"},
 	        {"Organized, orderly", "Flexible, adaptable"},
 		{"More outgoing, think out loud", "More reserved, think to yourself"},
+		{"Practical, realistic, frank", "Tactful, kind, encouraging"},
+

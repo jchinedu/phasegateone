7fdf0092d9baa8d75707cf7af1ab7ede2c47eee7
jchinedu <jchinedu080@gmail.com>
2025-06-12 06:18:41 +0100

    'progress'

--- python/test_mbti_test.py
+++ python/test_mbti_test.py
@@ -8,3 +8,6 @@ class TestMBTIFunctions(unittest.TestCase):
         	answers = "A" * 20
         	personality = mbtitest.process_answers(answers)
         	self.assertEqual(personality, "ESTJ")
+	def test_process_answers_invalid_length(self):
+        	with self.assertRaises(ValueError):
+            		mbtitest.process_answers("A" * 19)

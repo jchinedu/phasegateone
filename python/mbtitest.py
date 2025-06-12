beec0fe852fcf640d159ce7fd83dc43df05ddcae
jchinedu <jchinedu080@gmail.com>
2025-06-12 05:47:54 +0100

    'progress

--- python/mbtitest.py
+++ python/mbtitest.py
@@ -32,3 +32,5 @@ TRAITS = [
 def process_answers(answers):
         if len(answers) != len(QUESTIONS):
         	raise ValueError("Answers must be exactly 20 characters long.")
+	counts = {'E':0, 'I':0, 'S':0, 'N':0, 'T':0, 'F':0, 'J':0, 'P':0}
+

855b630406f4a5dda517d40752451136be7cfc01
jchinedu <jchinedu080@gmail.com>
2025-06-12 05:23:02 +0100

    'progress

--- python/mbtitest.py
+++ python/mbtitest.py
@@ -4,3 +4,4 @@ QUESTIONS = [
 	("Logical, thinking, questioning", "Empathetic, feeling, accommodating"),
 	("Organized, orderly", "Flexible, adaptable"),
 	("More outgoing, think out loud", "More reserved, think to yourself"),
+	("Practical, realistic, frank", "Tactful, kind, encouraging"),

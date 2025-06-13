88747af3fe5fb9951b0b3399f278d3d9e2d2db47
jchinedu <jchinedu080@gmail.com>
2025-06-13 17:06:06 +0100

    'progress'

--- python/student_grade.py
+++ python/student_grade.py
@@ -5,3 +5,12 @@ def calculate_totals(scores):
     return totals
 def calculate_averages(totals, number_of_subjects):
     return [total / number_of_subjects for total in totals]
+def check_position(totals):
+    ranks = []
+    for i in range(len(totals)):
+        rank = 1
+        for j in range(len(totals)):
+            if totals[j] > totals[i]:
+                rank += 1
+        ranks.append(rank)
+    return ranks

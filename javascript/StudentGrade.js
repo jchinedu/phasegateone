b1db3caf6853b4688f14310bab8d9dea67c8d644
jchinedu <jchinedu080@gmail.com>
2025-06-14 14:16:24 +0100

    'progress'

--- javascript/StudentGrade.js
+++ javascript/StudentGrade.js
@@ -21,3 +21,16 @@ function calculateAverages(totals, numberOfSubjects) {
   }
   return averages;
 }
+function checkPosition(totals) {
+  const ranks = [];
+  for (let i = 0; i < totals.length; i++) {
+    let rank = 1;
+    for (let j = 0; j < totals.length; j++) {
+      if (totals[j] > totals[i]) {
+        rank++;
+      }
+    }
+    ranks.push(rank);
+  }
+  return ranks;
+}

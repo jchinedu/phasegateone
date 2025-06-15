def3be13daa177873d06593f522a1aabe0a3d594
jchinedu <jchinedu080@gmail.com>
2025-06-15 14:55:07 +0100

    'progress'

--- python/phone_app.py
+++ python/phone_app.py
@@ -12,4 +12,17 @@ def remove_contact():
     global contacts
     contacts = [contact for contact in contacts if contact["phone_number"] != phone]
     print("Contact removed.")
-
+def find_contact_by_phone():
+    phone = input("Enter phone number to find: ")
+    for contact in contacts:
+        if contact["phone_number"] == phone:
+            print("Found:", contact["first_name"], contact["last_name"])
+            return
+    print("Contact not found.")
+def find_contact_by_first_name():
+    first = input("Enter first name to find: ")
+    for contact in contacts:
+        if contact["first_name"] == first:
+            print("Found:", ccontact["first_name"], contact["last_name"])
+            return
+    print("Contact not found.")

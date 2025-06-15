ce61e3136ba2a43a3a55548cf47d5f1e925e988a
jchinedu <jchinedu080@gmail.com>
2025-06-15 04:46:49 +0100

    'progress'

--- javascript/SemiColoncheckOut.js
+++ javascript/SemiColoncheckOut.js
@@ -1,3 +1,7 @@
 const prompt = require("prompt-sync")();
 console.log("Welcome to SemiColon Stores Checkout System!");
 let customerName = prompt("Enter customer name: ");
+let productNames = [];
+let quantities = [];
+let unitPrices = [];
+

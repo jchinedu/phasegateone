32e0fdb0990c5fb6917a3657936f70b5b29a67af
jchinedu <jchinedu080@gmail.com>
2025-06-15 10:05:20 +0100

    'progress'

--- javascript/PhoneBookApp.js
+++ javascript/PhoneBookApp.js
@@ -6,3 +6,6 @@ function addContact() {
   let firstName = prompt("Enter first name: ");
   let lastName = prompt("Enter last name: ");
   let phoneNumber = prompt("Enter phone number: ");
+  contacts.push({ firstName: firstName, lastName: lastName, phoneNumber: phoneNumber });
+  console.log("Contact added successfully.");
+}

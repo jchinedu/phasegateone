b6bc6b0601024585793a3e1b205fa6c3328123e8
jchinedu <jchinedu080@gmail.com>
2025-06-18 21:02:32 +0100

    'progress'

--- Java/PhoneBook.java
+++ Java/PhoneBook.java
@@ -15,5 +15,48 @@ System.out.print("Enter first name: ");
     contact.add(new String[]{fname, name, number});
     System.out.print("Contact added.");
 }
+public static void removeContact() {
+    System.out.print("Enter phone number to remove: ");
+    String phone = input.nextLine();
+
+    ArrayList<String[]> updatedContacts = new ArrayList<>();
+    boolean removed = false;
+
+    for (int i = 0; i < contacts.size(); i++) {
+        String[] contact = contacts.get(i);
+        if (!contact[2].equals(phone)) {
+            updatedContacts.add(contact);
+        } else {
+            removed = true;
+        }
+    }
+
+    contacts = updatedContacts;
+
+    if (removed) {
+        System.out.println("Contact removed.");
+    } else {
+        System.out.println("Contact not found.");
+    }
+}
+public static void findContactByPhone() {
+    System.out.print("Enter phone number to find: ");
+    String phone = input.nextLine();
+
+    String[] found = null;
+    for (int i = 0; i < contacts.size(); i++) {
+        String[] contact = contacts.get(i);
+        if (contact[2].equals(phone)) {
+            found = contact;
+            break;
+        }
+    }
+
+    if (found != null) {
+        System.out.println("Found: " + found[0] + " " + found[1]);
+    } else {
+        System.out.println("Contact not found.");
+    }
+}
 
 
